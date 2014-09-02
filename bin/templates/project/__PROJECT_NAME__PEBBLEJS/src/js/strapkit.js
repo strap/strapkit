4fcd1f1dad6b94676afc7a749b2e18d23739e3ff
marcelle bonterre <marcelle.bonterre@gmail.com>
2014-09-02 12:15:36 -0400

    working strapkit version

--- bin/templates/project/__PROJECT_NAME__PEBBLEJS/src/js/strapkit.js
+++ bin/templates/project/__PROJECT_NAME__PEBBLEJS/src/js/strapkit.js
@@ -1,41 +1,16 @@
 var UI = require('ui');
 var Accel = require('ui/accel');
-var Vector2 = require('vector2');
+Accel.init();
 
-var strapkit = { ui : {} };
-
-function strapkitPebbleMapGenerator(type,pebblejs){
-	if( 'ui' === type ){
-		return function(config){
-			return UI[pebblejs](config);
-		};
-	}
-	
-	return function(config){
-		return Accel[pebblejs](config);
-	};
-}
-
-(function(){
-	var uiMappings = {
-		'view' : 'Card',
-		'menu' : 'Menu',
-		'window' : 'Window',
-		'text' : 'Text'
-	};
-	var strapkitMethod, pebblejs;
-	for(strapkitMethod in uiMappings ){
-		pebblejs = uiMappings[strapkitMethod];
-		strapkit.ui[strapkitMethod] = strapkitPebbleMapGenerator( 'ui', pebblejs );
-	}
-	
-	Accel.init();
-	var sensorMappings = {};
-	for(strapkitMethod in sensorMappings ){
-		pebblejs = sensorMappings[strapkitMethod];
-		strapkit.ui[strapkitMethod] = strapkitPebbleMapGenerator( 'accel', pebblejs );
-	}
-
-}());
-
-module.exports = strapkit; 
+module.exports = { 
+	'ui' : {
+		'view' : function(config){ return new UI.Card(config); },
+		'menu' : function(config){ return new UI.Menu(config); },
+		'window' : function(config){ return new UI.Window(config); },
+		'text' : function(config){ return new UI.Text(config); },
+		'vibe' : function(config){ return require('ui/vibe'); }
+	},
+	'sensors' : {
+		'accel' : function(){ return Accel; }
+	},
+}; 
