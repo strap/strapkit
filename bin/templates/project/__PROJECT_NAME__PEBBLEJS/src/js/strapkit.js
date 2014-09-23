4c15759078be8b16ed4a8c89e6c7a2c2a03d404b
marcelle bonterre <marcelle.bonterre@gmail.com>
2014-09-23 12:07:36 -0400

    strapkit copies all project files into internal pebblejs dir

--- bin/templates/project/__PROJECT_NAME__PEBBLEJS/src/js/strapkit.js
+++ bin/templates/project/__PROJECT_NAME__PEBBLEJS/src/js/strapkit.js
@@ -2,17 +2,32 @@ var UI = require('ui');
 var Accel = require('ui/accel');
 Accel.init();
 
-module.exports = { 
-	'ui' : {
-		'view' : function(config){ return new UI.Card(config); },
-		'menu' : function(config){ return new UI.Menu(config); },
-		'window' : function(config){ return new UI.Window(config); },
-		'text' : function(config){ return new UI.Text(config); },
-		'vibe' : function(config){ return require('ui/vibe'); }
-	},
+// module.exports = {
+// 	'ui' : {
+// 		'view' : function(config){ return new UI.Card(config); },
+// 		'menu' : function(config){ return new UI.Menu(config); },
+// 		'window' : function(config){ return new UI.Window(config); },
+// 		'text' : function(config){ return new UI.Text(config); },
+// 		'vibe' : function(config){ return require('ui/vibe'); }
+// 	},
+// 	'sensors' : {
+// 		'accel' : function(){ return Accel; }
+// 	},
+// 	'settings' : function(){ return require('settings'); },
+// 	'ajax' : function(){ return require('ajax'); }
+// }; 
+
+UI.view = function(config){ return new UI.Card(config); };
+UI.menu = function(config){ return new UI.Menu(config); };
+UI.window = function(config){ return new UI.Window(config); };
+UI.text = function(config){ return new UI.Text(config); };
+UI.vibe = function(config){ return require('ui/vibe'); };
+
+module.exports = {
+	'ui' : UI,
 	'sensors' : {
 		'accel' : function(){ return Accel; }
 	},
 	'settings' : function(){ return require('settings'); },
 	'ajax' : function(){ return require('ajax'); }
-}; 
+}
