d21cf7059ecefb07783787e09150aa44ab04ca25
marcelle bonterre <marcelle.bonterre@gmail.com>
2014-10-14 14:50:52 -0400

    updated sample to strapkit new api methods

--- sample/sample.js
+++ sample/sample.js
@@ -1,24 +1,23 @@
-var Vector2 = require('vector2');
 var SK = require('strapkit');
-var UI = SK.ui;
-var SM = SK.metrics;
-SM.init({
+var UI = SK.UI;
+var SM = SK.Metrics;
+SM.Init({
     app_id: "jNW93sNJpsgTGiPQF",
     resolution: "144x168",
     useragent: "PEBBLE/2.0"
 });
 
-var main = UI.view({
+var main = UI.View({
   title: 'Strapkit',
   subtitle: 'Hello World!',
   body: 'Press any button.'
 });
 
 main.show();
-SM.log('/main/show');
+SM.Log('/main/show');
 
 main.on('click', 'up', function(e) {
-  var menu =  UI.menu({
+  var menu =  UI.Menu({
     sections: [{
       items: [{
         title: 'Strapkit',
@@ -30,32 +29,32 @@ main.on('click', 'up', function(e) {
     }]
   });
   menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-    SM.log('/selected/'+e.item.title);
+    console.Log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.Log('The item is titled "' + e.item.title + '"');
+    SM.Log('/selected/'+e.item.title);
   });
   menu.show();
 });
 
 main.on('click', 'select', function(e) {
-  var wind =  UI.window();
-  var textfield =  UI.text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
+  var wind =  UI.Window();
+  var Textfield =  UI.Text({
+    position: SK.Coord(0, 50),
+    size: SK.Coord(144, 30),
     font: 'gothic-24-bold',
     text: 'Text Anywhere!',
-    textAlign: 'center'
+    TextAlign: 'center'
   });
-  wind.add(textfield);
+  wind.add(Textfield);
   wind.show();
-  SM.log('/main/select');
+  SM.Log('/main/select');
 });
 
 main.on('click', 'down', function(e) {
-  var card = UI.view();
+  var card = UI.View();
   card.title('A Card');
   card.subtitle('Is a Window');
-  card.body('The simplest window type in Strapkit.');
+  card.body('The simplest Window type in Strapkit.');
   card.show();
-  SM.log('/main/down');
+  SM.Log('/main/down');
 });
